f24519fa105c77954ebd243f5abcd8a3a7707082
Glitch (neroio2) <none>
2023-10-24 12:02:55 +0000

    🕍🌒 Checkpoint ./server/config.js:31612930/8

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "neroio2.glitch.me",
+    host: "https://neroio2.glitch.me",
 
     // Which port to run the web server on.
     port: 3000,
