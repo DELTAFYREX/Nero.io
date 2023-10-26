38608fae8b2fded4c20941dd9c6203ac2137987f
Glitch (neroio2) <none>
2023-10-26 12:22:09 +0000

    🚟🐴 Checkpoint ./public/index.html:31612930/188 ./public/app.js:31612930/2899

--- public/app.js
+++ public/app.js
@@ -121,7 +121,7 @@ fetch("changelog.html", { cache: "no-cache" })
         const cache = {};
         let i = 0;
         for (let image of [ // MUST BE PNG
-            ["./resources/IED.png", "ied"],
+            ["./images/lratio.png", "lratio"],
         ]) {
             setTimeout(() => {
                 if (image[2]) {
@@ -652,8 +652,8 @@ function drawPoly(context, centerX, centerY, radius, sides, angle = 0, borderles
     } else if (sides > 10000) {
       switch (sides) {
       case 10001:
-          if (!_imageCache.do_not_open_at_any_cost || !_imageCache.do_not_open_at_any_cost.ready) break;
-          context.drawImage(_imageCache.do_not_open_at_any_cost, -radius, -radius, radius * 2, radius * 2);
+          if (!_imageCache.lratio || !_imageCache.lratio.ready) break;
+          context.drawImage(_imageCache.lratio, -radius, -radius, radius * 2, radius * 2);
           break;
       }
 }
@@ -818,9 +818,45 @@ function drawHealth(x, y, instance, ratio, alpha) {
             drawText(name, x, y - realSize - 30, 16, namecolor, "center");
             drawText(util.handleLargeNumber(instance.score, 1), x, y - realSize - 16, 8, namecolor, "center");
             ctx.globalAlpha = 1;
-        }
-    }
-}
+                          drawText(instance.score > 0 ? _util._handleLargeNumber(instance.score) : "", x, y - realSize - 16 * nameRatio, 8 * nameRatio, "#E4EBE7", "center", false, 1, stroke, ctx, font);
+                switch (fill.charAt(0)) {
+                    case "!":
+                        let data = _util._getSpecialNameInfoById(Number(instance.nameColor.substring(1)));
+                        fill = data[0];
+                        stroke = data[1];
+                        font = data[2];
+                        imageRatio = data[3];
+                        badge = data[4];
+                        break;
+                }
+                          // TODO: make these use oblivion's gradient system
+                if (instance.name.includes("U S A")) {
+                    drawText("U", x - 14, y - realSize - 30, 16, color.red, "center");
+                    drawText("S", x, y - realSize - 30, 16, color.guiwhite, "center");
+                    drawText("A", x + 14, y - realSize - 30, 16, color.blue, "center");
+                } else if (instance.name.includes("Trans Rights")) {
+                    drawText("T", x - 50, y - realSize - 30, 16, "#5BCEF5", "center");
+                    drawText("r", x - 41, y - realSize - 30, 16, "#F5A8B4", "center");
+                    drawText("a", x - 32, y - realSize - 30, 16, "#FFFFFF", "center");
+                    drawText("n", x - 23, y - realSize - 30, 16, "#F5A8B4", "center");
+                    drawText("s", x - 14, y - realSize - 30, 16, "#5BCEF5", "center");
+                    drawText("", x - 5, y - realSize - 30, 16, "#5BCEF5", "center");
+                    drawText("R", x + 5, y - realSize - 30, 16, "#5BCEF5", "center");
+                    drawText("i", x + 14, y - realSize - 30, 16, "#F5A8B4", "center");
+                    drawText("g", x + 23, y - realSize - 30, 16, "#FFFFFF", "center");
+                    drawText("h", x + 32, y - realSize - 30, 16, "#FFFFFF", "center");
+                    drawText("t", x + 41, y - realSize - 30, 16, "#F5A8B4", "center");
+                    drawText("s", x + 50, y - realSize - 30, 16, "#5BCEF5", "center");
+                } else {
+                    drawText(instance.name, x, y - realSize - 30 * nameRatio, 16 * nameRatio, fill, "center", false, 1, stroke, ctx, font);
+                };
+                if (badge && _imageCache[badge] && _imageCache[badge].ready) {
+                    let size = 18 * nameRatio * imageRatio;
+                    ctx.drawImage(_imageCache[badge], x - size * 1.1 - measureText(instance.name, 16 * nameRatio, false, font) / 2, y - realSize - 30 * nameRatio - size * 0.75, size, size);
+                }
+                ctx.globalAlpha = 1;
+            }
+        } 
 // Start animation
 window.requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || (callback => setTimeout(callback, 1000 / 60));
 window.cancelAnimFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
