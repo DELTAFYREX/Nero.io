816c9b6ad8c05b7274d2f27f3426c89d1f3cd611
Glitch (neroio2) <none>
2023-11-28 16:20:41 +0000

    🗽☕️ Checkpoint ./public/app.js:31612930/112

--- public/app.js
+++ public/app.js
@@ -753,7 +753,7 @@ const drawEntity = (baseColor, x, y, instance, ratio, alpha = 1, scale = 1, rot 
             drawTrapezoid(context, xx + drawSize * gx, yy + drawSize * gy, drawSize * (g.length / 2 - (g.aspect === 1 ? position * 2 : 0)), (drawSize * g.width) / 2, g.aspect, g.angle + rot, borderless, fill);
         }
       }
-    } else if (m.shape > 999) {
+    } else if (m.sides > 999) {
                         context.save();
                         context.beginPath();
                         context.translate(m.centerX, m.centerY);
@@ -761,7 +761,7 @@ const drawEntity = (baseColor, x, y, instance, ratio, alpha = 1, scale = 1, rot 
                         if (!window.frames) {
                             window.frames = {}
                         }
-        switch (m.shape) {
+        switch (m.sides) {
           case 1001:
           if (!_imageCache.ied || !_imageCache.ied.ready) break;
           context.drawImage(_imageCache.ied, -m.radius * 1.5, -m.radius * 2.5, m.radius * 3, m.radius * 5);
