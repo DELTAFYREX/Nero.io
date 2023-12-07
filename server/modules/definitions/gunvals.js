afdb272d8bc5e1b6f6dffef6ffc5e90c365ecbcb
Glitch (neroio2) <none>
2023-12-07 14:12:37 +0000

    👼🌚 Checkpoint ./server/modules/definitions/gunvals.js:31612930/37

--- server/modules/definitions/gunvals.js
+++ server/modules/definitions/gunvals.js
@@ -145,5 +145,5 @@ module.exports = {
     acc: { shudder: 0.1 },
     aura: { reload: 0.001, recoil: 0.001, shudder: 0.001, size: 6, damage: 3, speed: 0.001, maxSpeed: 0.001, spray: 0.001 },
     noRandom: { shudder: 0, spray: 0 },
-    lance: { reload: 6, recoil: 0, shudder: 0.1, size: 1, health: 0.5, damage: 1.2, pen: 1.6, speed: 0.67, maxSpeed: 1, range: 0.08, density: 1, spray: 180, resist: 1},
+    lance: { reload: 6, recoil: 0, shudder: 0.1, size: 1, health: 0.1, damage: 1, pen: 1.2, speed: 1.5, maxSpeed: 2, range: 0.01, density: 1, spray: 180, resist: 1},
 }
