a10ac033942e2f6d5f31b5dba900653dabecb9e0
Glitch (neroio2) <none>
2023-12-11 15:27:28 +0000

    🏢📷 Checkpoint ./public/app.js:31612930/8278

--- public/app.js
+++ public/app.js
@@ -1598,7 +1598,7 @@ function drawMinimapAndDebug(spacing, alcoveSize) {
         drawGuiRect(x, y - 40, len, 30);
         lagGraph(lag.get(), x, y - 40, len, 30, color.teal);
         gapGraph(global.metrics.rendergap, x, y - 40, len, 30, color.pink);
-        timingGraph(x, y - 40, len, 30, color.yellow);
+        //timingGraph(GRAPHDATA, x, y - 40, len, 30, color.yellow);
     }
     //minimap stuff ends here
     //debug stuff
@@ -1606,8 +1606,8 @@ function drawMinimapAndDebug(spacing, alcoveSize) {
     // Text
     if (global.showDebug) {
         drawText("Nero.io 2", x + len, y - 50 - 5 * 14 - 2, 15, "#B6E57C", "right");
-        drawText("Prediction: " + "ms", x + len, y - 50 - 4 * 14, 10, color.guiwhite, "right");
-        drawText(`Bandwidth: ${gui.bandwidth.in} in, ${gui.bandwidth.out} out`, x + len, y - 50 - 3 * 14, 10, color.guiwhite, "right");
+        //drawText("Prediction: " + Math.round(GRAPHDATA) + "ms", x + len, y - 50 - 4 * 14, 10, color.guiwhite, "right");
+        //drawText(`Bandwidth: ${gui.bandwidth.in} in, ${gui.bandwidth.out} out`, x + len, y - 50 - 3 * 14, 10, color.guiwhite, "right");
         drawText("Update Rate: " + global.metrics.updatetime + "Hz", x + len, y - 50 - 2 * 14, 10, color.guiwhite, "right");
         drawText((100 * gui.fps).toFixed(2) + "% : " + global.metrics.rendertime + " FPS", x + len, y - 50 - 1 * 14, 10, global.metrics.rendertime > 10 ? color.guiwhite : color.orange, "right");
         drawText(global.metrics.latency + " ms - " + global.serverName, x + len, y - 50, 10, color.guiwhite, "right");
@@ -1912,6 +1912,9 @@ function animloop() {
             // Do rendering speed.
             global.metrics.rendertime = renderTimes;
             renderTimes = 0;
+            // Do Bandwidth.
+            global.bandwidth.in = global.bandwidth.out;
+            global.bandwidth.out = 0;
             // Do update rate.
             global.metrics.updatetime = global.updateTimes;
             global.updateTimes = 0;
