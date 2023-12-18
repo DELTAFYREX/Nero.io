e09f0f9bce58e71f70e60553c0f7b74ae8b6f8a0
Glitch (neroio2) <none>
2023-12-18 18:11:27 +0000

    🚗🎤 Checkpoint ./server/lib/dotenv.js:31612930/290 ./server/config.js:31612930/2

--- server/lib/dotenv.js
+++ server/lib/dotenv.js
@@ -1,5 +1,21 @@
+
+if (typeof Object.fromEntries !== 'function') {
+
+    console.log("\nUpdate your Node.js, it's hella outdated.\n");
+
+    Object.fromEntries = entries => {
+
+        let result = {};
+
+        for (let [key, value] of entries) result[key] = value;
+
+        return result;
+
+    };
+
+}
 //This module parses .env file data
-module.exports = data => Object.prototype.fromEntries(data.split(/\r?\n/g).map(line => {
+module.exports = data => Object.fromEntries(data.split(/\r?\n/g).map(line => {
     if (!line.includes('=') || line.trim().startsWith('#')) return null;
     const key = line.slice(0, line.indexOf('='));
     const value = line.slice(line.indexOf('=') + 1);
