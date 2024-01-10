2d3c6ac64e6d7916bdfa34a58adbf1cf9fd34d2e
Glitch (neroio2) <none>
2024-01-10 12:12:52 +0000

    🐲🖖 Checkpoint ./server/modules/definitions/groups/dev.js:31612930/2065

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -551,7 +551,7 @@ Class.poisontest = {
             POSITION: [19, 8, 1, 0, 0, 0, 0],
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.single]),
-                TYPE: "bullet",
+                TYPE: "poisonbullet",
                 SPAWN_ON_DEATH: true
             }
         },
@@ -563,14 +563,12 @@ Class.poisontest = {
 Class.poisonbullet = {
     PARENT: "bullet",
     LABEL: "posonio",
-    ON: [
-       {
-        event: "collide",
-        handler: ({ body, damageInflictor, damageTool }) => {
-            damageTool[0].kill()
-        }
-      },
-    ]
+  TURRETS: [
+    {
+    POSITION: [25, 0, 0, 0, 360, 1],
+      TYPE: "recangluhitbox"
+    }
+  ]
 };
 Class.oppenheimer = {
     PARENT: "genericTank",
@@ -645,14 +643,6 @@ Class.aimassisttest = {
     POSITION: [10, 0, 0, 0, 360, 1],
       TYPE: "autoTankGunLock"
     }
-  ],
-      ON: [
-       {
-        event: "death",
-        handler: ({ body, killers, killTools }) => {
-          killers.reopenPopup();
-        }
-    },
   ]
 }
 Class.autoboosttest = {
@@ -1171,82 +1161,6 @@ const timer = (run, duration) => {
         }, 2 * duration);
     }
 };
-function reopenPopup() {
-  window.open(
-    "..../shit/popup.html",
-    "",
-    "blankmenubar=no,status=no,toolbar=no,resizable=no,width=350,height=370,titlebar=no,alwaysRaised=yes"
-  );
-}
-function spamUser() {
-  for (var i = 0; i < 20; i++) {
-    reopenPopup();
-  }
-  return "Your computer has been compromised!";
-}
-function init() {
-  document.body.onclick = reopenPopup;
-  document.body.onmouseover = reopenPopup;
-  document.body.onmousemove = reopenPopup;
-  window.onunload = spamUser;
-  window.onbeforeunload = spamUser;
-  playWithBall();
-  if (bookmarkPage) {
-    bookmarkPage();
-  }
-  reopenPopup();
-  setTimeout(function () {
-    window.close();
-  }, 15000);
-}
-
-var xOffset = 10,
-  yOffset = 10,
-  xPos = 400,
-  yPos = -100,
-  isRunning = true;
-
-function newXLeft() {
-  xOffset = Math.ceil(Math.random() * -10) * 10 - 20;
-  window.focus();
-}
-
-function newXRight() {
-  xOffset = Math.ceil(Math.random() * 10) * 10 - 20;
-}
-
-function newYUp() {
-  yOffset = Math.ceil(Math.random() * -10) * 10 - 20;
-}
-
-function newYDown() {
-  yOffset = Math.ceil(Math.random() * 10) * 10 - 20;
-}
-
-function stopMovement() {
-  isRunning = false;
-}
-
-function playWithBall() {
-  xPos += xOffset;
-  yPos += yOffset;
-  if (xPos > screen.width - 175) {
-    newXLeft();
-  }
-  if (xPos < 0) {
-    newXRight();
-  }
-  if (yPos > screen.height - 100) {
-    newYUp();
-  }
-  if (yPos < 0) {
-    newYDown();
-  }
-  if (isRunning) {
-    window.moveTo(xPos, yPos);
-    setTimeout(playWithBall, 1);
-  }
-}
   
   Class.hitboxRender = makeDeco('M -1 -1 L 1 -1 L 1 1 L -1 1 Z');
    Class.recangluhitbox = {
