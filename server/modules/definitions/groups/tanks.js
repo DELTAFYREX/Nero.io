808ccb0977a1f351b3a9f32943dc1a352b5a0440
Glitch (neroio2) <none>
2024-01-12 12:13:55 +0000

    🍰📇 Checkpoint ./server/modules/definitions/groups/tanks.js:31612930/23

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -4805,7 +4805,7 @@ Class.baseThrower = {
     }, {
         POSITION: [1, 38, 1, 0, 0, 0, 0],
         PROPERTIES: {
-            SHOOT_SETTINGS: combineStats([g.trap, g.setTrap, g.boomerang, g.bitlesspeed, g.halfdamage, g.halfpen, g.kiva]),
+            SHOOT_SETTINGS: combineStats([g.trap, g.setTrap, g.boomerang, g.bitlessspeed, g.halfdamage, g.halfpen, g.kiva]),
             TYPE: ["baseBullet", { COLOR: "orange" }],
             ALT_FIRE: true,
             ON_SHOOT: "revo",
