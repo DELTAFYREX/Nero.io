82145f1735380e9d3a87c7ad0afe53ac4c80ccba
Glitch (neroio2) <none>
2024-01-18 18:47:42 +0000

    ✍️🐆 Checkpoint ./public/app.js:31612930/6041 ./public/index.html:31612930/208 ./public/main.css:31612930/328

--- public/app.js
+++ public/app.js
@@ -163,57 +163,9 @@ function PlaySound169() {
       if (document.getElementById("optSound").checked === true) {
            global.music2.play()
     global.music2.addEventListener('ended', function() {this.currentTime = 0; this.play();
-                                                                   var context = new AudioContext();
-    var src = context.createMediaElementSource(audio);
-    var analyser = context.createAnalyser();
+window.onload = function() {
 
-    var canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    var ctx = canvas.getContext("2d");
-
-    src.connect(analyser);
-    analyser.connect(context.destination);
-
-    analyser.fftSize = 256;
-
-    var bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
-
-    var dataArray = new Uint8Array(bufferLength);
-
-    var WIDTH = canvas.width;
-    var HEIGHT = canvas.height;
-
-    var barWidth = (WIDTH / bufferLength) * 2.5;
-    var barHeight;
-    var x = 0;
-
-    function renderFrame() {
-      requestAnimationFrame(renderFrame);
-
-      x = 0;
-
-      analyser.getByteFrequencyData(dataArray);
-
-      ctx.fillStyle = "#000000";
-      ctx.fillRect(0, 0, WIDTH, HEIGHT);
-
-      for (var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-        
-        var r = barHeight + (25 * (i/bufferLength));
-        var g = 250 * (i/bufferLength);
-        var b = 50;
-
-        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
-        x += barWidth + 1;
-      }
-    }
-    renderFrame();}, false);
-     } else if (document.getElementById("optSound").checked === false) {
+}}}}else if (document.getElementById("optSound").checked === false) {
           global.music2.pause()
             }
          return; };
