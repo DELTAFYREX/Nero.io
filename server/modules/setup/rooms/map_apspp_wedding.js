4af590e14dcd7f65ceea6f64504b775ed4de77f7
Glitch (neroio2) <none>
2024-01-20 16:06:29 +0000

    🎍🐪 Checkpoint ./server/modules/setup/rooms/map_apspp_wedding.js:31612930/378

--- server/modules/setup/rooms/map_apspp_wedding.js
+++ server/modules/setup/rooms/map_apspp_wedding.js
@@ -3,27 +3,27 @@ let { rock, roid, blackrock: blrk, blackroid: blrd } = require('../tiles/decorat
     { portal: port } = require('../tiles/portal.js'),
 
 room = [
-    [rock,rock,roid,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,roid,rock,rock],
+    [rock,rock,rock,rock,roid,roid,____,____,____,____,____,____,____,____,____,roid,roid,rock,rock,rock,rock],
     [rock,port,port,____,rock,____,____,roid,rock,rock,rock,rock,rock,roid,____,____,rock,____,port,port,rock],
-    [roid,port,port,____,rock,____,____,____,____,____,____,____,____,____,____,____,rock,____,port,port,roid],
-    [____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____],
-    [____,rock,rock,____,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,____,rock,rock,____],
-    [____,____,____,____,nest,____,____,____,____,____,____,____,____,____,____,____,nest,____,____,____,____],
+    [rock,port,port,____,rock,____,____,____,____,____,____,____,____,____,____,____,rock,____,port,port,rock],
+    [rock,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,rock],
+    [roid,rock,rock,____,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,____,rock,rock,roid],
+    [roid,____,____,____,nest,____,____,____,____,____,____,____,____,____,____,____,nest,____,____,____,roid],
     [____,____,____,____,nest,____,WALL,WALL,WALL,WALL,____,WALL,WALL,WALL,WALL,____,nest,____,____,____,____],
     [____,____,____,____,nest,____,WALL,blrk,blrd,blat,blat,blat,blrd,blrk,WALL,____,nest,____,____,____,____],
-    [____,____,____,____,nest,____,WALL,blrd,dcf4,dcf2,dcf0,dcf4,dcf0,blrd,WALL,____,nest,____,____,____,____],
-    [____,____,____,____,nest,____,WALL,blat,dcf5,dcf0,dcf0,dcf1,dcf5,blat,WALL,____,nest,____,____,____,____],
-    [____,roid,roid,____,nest,____,____,blat,dcf2,dcf3,dcf2,dcf5,dcf4,blat,____,____,nest,____,roid,roid,____],
-    [____,____,____,____,nest,____,WALL,blat,dcf3,dcf1,dcf4,dcf2,dcf5,blat,WALL,____,nest,____,____,____,____],
-    [____,____,____,____,nest,____,WALL,blrd,dcf1,dcf4,dcf1,dcf0,dcf2,blrd,WALL,____,nest,____,____,____,____],
+    [rock,____,____,____,nest,____,WALL,blrd,dcf4,dcf2,dcf0,dcf4,dcf0,blrd,WALL,____,nest,____,____,____,rock],
+    [rock,____,____,____,nest,____,WALL,blat,dcf5,dcf0,dcf0,dcf1,dcf4,blat,WALL,____,nest,____,____,____,rock],
+    [rock,roid,roid,____,nest,____,____,blat,dcf2,dcf3,dcf2,dcf5,dcf4,blat,____,____,nest,____,roid,roid,rock],
+    [rock,____,____,____,nest,____,WALL,blat,dcf3,dcf1,dcf4,dcf2,dcf5,blat,WALL,____,nest,____,____,____,rock],
+    [rock,____,____,____,nest,____,WALL,blrd,dcf1,dcf4,dcf1,dcf0,dcf2,blrd,WALL,____,nest,____,____,____,rock],
     [____,____,____,____,nest,____,WALL,blrk,blrd,blat,blat,blat,blrd,blrk,WALL,____,nest,____,____,____,____],
     [____,____,____,____,nest,____,WALL,WALL,WALL,WALL,____,WALL,WALL,WALL,WALL,____,nest,____,____,____,____],
-    [____,____,____,____,nest,____,____,____,____,____,____,____,____,____,____,____,nest,____,____,____,____],
-    [____,rock,rock,____,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,____,rock,rock,____],
-    [____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____],
-    [roid,port,port,____,rock,____,____,____,____,____,____,____,____,____,____,____,rock,____,port,port,roid],
+    [roid,____,____,____,nest,____,____,____,____,____,____,____,____,____,____,____,nest,____,____,____,roid],
+    [roid,rock,rock,____,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,nest,____,rock,rock,roid],
+    [rock,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,rock],
+    [rock,port,port,____,rock,____,____,____,____,____,____,____,____,____,____,____,rock,____,port,port,rock],
     [rock,port,port,____,rock,____,____,roid,rock,rock,rock,rock,rock,roid,____,____,rock,____,port,port,rock],
-    [rock,rock,roid,____,____,____,____,____,____,____,____,____,____,____,____,____,____,____,roid,rock,rock]
+    [rock,rock,rock,rock,roid,roid,____,____,____,____,____,____,____,____,____,roid,roid,rock,rock,rock,rock]
 ];
 
 module.exports = room;
