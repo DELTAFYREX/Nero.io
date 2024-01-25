332aefe9c7a7e21be43d32bfeb96f9b5d067e16a
Glitch (neroio2) <none>
2024-01-25 13:56:25 +0000

    Rewound to commit fbc211e9b90cb2f93f958f2a18425a0ad48c40ec + 0 edits

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -5527,4 +5527,4 @@ Class.basic.UPGRADES_TIER_1 = ["twin", "sniper", "machineGun", "flankGuard", "di
 
     Class.desmos.UPGRADES_TIER_2 = ["volute", "helix", "autoDesmos", "desmosbrid"]
         Class.volute.UPGRADES_TIER_3 = ["sidewinder", "autoVolute", "volutebrid"]
-        Class.helix.UPGRADES_TIER_3 = ["triplex", "quadruplex", "autoHybrid"]
+        Class.helix.UPGRADES_TIER_3 = ["triplex", "quadruplex", "autoHelix", "helixbrid"]
