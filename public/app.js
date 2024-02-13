01d38929020f4e7b447d4b5713648b133dbed622
Glitch (neroio2) <none>
2024-02-13 12:25:02 +0000

    🛣🍛 Checkpoint ./public/app.js:31612930/513 ./changelog preview:31612930/192

--- public/app.js
+++ public/app.js
@@ -1888,12 +1888,13 @@ function drawMinimapAndDebug(spacing, alcoveSize) {
     if (!global.showDebug) y += 14 * 3;
     // Text
     if (global.showDebug) {
-        drawText("Nero Engine v2.9", x + len, y - 50 - 6 * 14 - 2, 15, "#B6E57C", "right");
+        drawText("Nero Engine v2.93", x + len, y - 50 - 7 * 14 - 2, 15, "#B6E57C", "right");
         //drawText("Prediction: " + Math.round(GRAPHDATA) + "ms", x + len, y - 50 - 4 * 14, 10, color.guiwhite, "right");
+        drawText("Update Version: " + "v2.9346", x + len, y - 50 - 6 * 14, 10, color.guiwhite, "right");
         drawText("Update Rate: " + global.metrics.updatetime + "Hz", x + len, y - 50 - 5 * 14, 10, color.guiwhite, "right");
-        drawText("Update Version: " + "2.9114", x + len, y - 50 - 4 * 14, 10, color.guiwhite, "right");
-        drawText("Client Speed: " + global.metrics.rendertime + " FPS", x + len, y - 50 - 3 * 14, 10, global.metrics.rendertime > 10 ? color.guiwhite : color.orange, "right");
-        drawText("Server Speed: " + ((global.metrics.updatetime * global.metrics.rendergap-global.metrics.lag) / 10).toFixed(2) + "%", x + len, y - 50 - 2 * 14, 10, color.guiwhite, "right");
+        drawText("Client Speed: " + global.metrics.rendertime + " FPS", x + len, y - 50 - 4 * 14, 10, global.metrics.rendertime > 10 ? color.guiwhite : color.orange, "right");
+        drawText("Server Speed: " + ((global.metrics.updatetime * global.metrics.rendergap-global.metrics.lag) / 10).toFixed(2) + "%", x + len, y - 50 - 3 * 14, 10, color.guiwhite, "right");
+        drawText("Kills: " + global.metrics.killcount, x + len, y - 50 - 2 * 14, 10, color.guiwhite, "right");
         drawText("Song: " + global.music2.songname, x + len, y - 50 - 1 * 14, 10, color.guiwhite, "right");
         drawText(global.metrics.latency + " ms - neroio2 :FFA:", x + len, y - 50, 10, color.guiwhite, "right");
     } else {
@@ -2223,6 +2224,7 @@ function animloop() {
         }
         if (global.died) {
             gameDrawDead();
+            global.metrics.killcount = 0;
         }
         if (global.disconnected) {
             gameDrawDisconnected();
