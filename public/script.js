15529b9ff2b3ca2db0054226a56016529bdc98aa
Glitch (neroio2) <none>
2024-02-13 19:01:04 +0000

    🚅✍️ Checkpoint ./public/script.js:31612930/30520 ./public/stylesheet.css:31612930/2758 ./public/visual.html:31612930/2464

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
   
   //Music functions:
   //decide the music
-  var music2 = document.getElementById("audio");
+  varzmusic2 = new Audio();
   const pmusic = ["https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/oioioi.mp3?v=1705286830033", "https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/4Miklipi%20(Dejected)%20Preview.mp3?v=1705287022417", "https://cdn.glitch.me/5fc7dcb6-aada-495b-828e-66901a470a29/World's%20End.wav?v=1705286889038", "https://cdn.glitch.global/f80d3eec-1e99-4b8c-b120-79a55addacf9/Meloncholy.mp3?v=1675465750213","https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/Depredation.mp3?v=1705286866890", "https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/videoplayback.mp3?v=1705807057028"];
   var randmusic = pmusic[~~(Math.random() * pmusic.length)];
   music2.src = (randmusic);
@@ -15,22 +15,24 @@ window.onload = function() {
 //actually play the audio when the checkbox is clicked on (checked) and stop it when unchecked
     document.getElementById("optSound").onclick = () => {
       if (document.getElementById("optSound").checked === true) {
-           music2.play()
-    music2.addEventListener('ended', function() {this.currentTime = 0; music2.src = pmusic[~~(Math.random() * pmusic.length)]; this.play();}, false);
+           global.music2.play()
+    global.music2.addEventListener('ended', function() {this.currentTime = 0; global.music2.src = pmusic[~~(Math.random() * pmusic.length)]; this.play();}, false);
      } else if (document.getElementById("optSound").checked === false) {
-          music2.pause()
+          global.music2.pause()
+          global.music2.songname = "Not Playing";
             }
          return; };
   
   var file = document.getElementById("thefile");
-  //var audio = document.getElementById("audio");
+  var audio = document.getElementById("audio");
   
+  file.onchange = function() {
     var files = this.files;
-    //audio.src = randmusic
-    //audio.load();
-    //audio.play();
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    audio.play();
     var context = new AudioContext();
-    var src = context.createMediaElementSource(music2);
+    var src = context.createMediaElementSource(audio);
     var analyser = context.createAnalyser();
 
     var canvas = document.getElementById("canvas");
@@ -79,6 +81,7 @@ window.onload = function() {
       }
     }
 
-    music2.play();
+    audio.play();
     renderFrame();
   };
+};
