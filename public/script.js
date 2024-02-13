0a08438fc09f59c449f044f3177d0df1b0adb144
Glitch (neroio2) <none>
2024-02-13 18:49:56 +0000

    🐇🎇 Checkpoint ./public/script.js:31612930/486 ./public/visual.html:31612930/2490 ./public/stylesheet.css:31612930/42

--- public/script.js
+++ public/script.js
@@ -2,37 +2,35 @@ window.onload = function() {
   
   //Music functions:
   //decide the music
-  global.music2 = new Audio();
+  var music2 = document.getElementById("audio");
   const pmusic = ["https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/oioioi.mp3?v=1705286830033", "https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/4Miklipi%20(Dejected)%20Preview.mp3?v=1705287022417", "https://cdn.glitch.me/5fc7dcb6-aada-495b-828e-66901a470a29/World's%20End.wav?v=1705286889038", "https://cdn.glitch.global/f80d3eec-1e99-4b8c-b120-79a55addacf9/Meloncholy.mp3?v=1675465750213","https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/Depredation.mp3?v=1705286866890", "https://cdn.glitch.global/5fc7dcb6-aada-495b-828e-66901a470a29/videoplayback.mp3?v=1705807057028"];
   var randmusic = pmusic[~~(Math.random() * pmusic.length)];
-  global.music2.src = (randmusic);
+  music2.src = (randmusic);
   //load the play functions for itasdasf meow
   function PlayMusic() {
-  global.music2.load();   
-  global.music2.play();
+  music2.load();   
+  music2.play();
 }
 
 //actually play the audio when the checkbox is clicked on (checked) and stop it when unchecked
     document.getElementById("optSound").onclick = () => {
       if (document.getElementById("optSound").checked === true) {
-           global.music2.play()
-    global.music2.addEventListener('ended', function() {this.currentTime = 0; global.music2.src = pmusic[~~(Math.random() * pmusic.length)]; this.play();}, false);
+           music2.play()
+    music2.addEventListener('ended', function() {this.currentTime = 0; music2.src = pmusic[~~(Math.random() * pmusic.length)]; this.play();}, false);
      } else if (document.getElementById("optSound").checked === false) {
-          global.music2.pause()
-          global.music2.songname = "Not Playing";
+          music2.pause()
             }
          return; };
   
   var file = document.getElementById("thefile");
-  var audio = document.getElementById("audio");
+  //var audio = document.getElementById("audio");
   
-  file.onchange = function() {
     var files = this.files;
-    audio.src = URL.createObjectURL(files[0]);
-    audio.load();
-    audio.play();
+    //audio.src = randmusic
+    //audio.load();
+    //audio.play();
     var context = new AudioContext();
-    var src = context.createMediaElementSource(audio);
+    var src = context.createMediaElementSource(music2);
     var analyser = context.createAnalyser();
 
     var canvas = document.getElementById("canvas");
@@ -81,7 +79,6 @@ window.onload = function() {
       }
     }
 
-    audio.play();
+    music2.play();
     renderFrame();
   };
-};
