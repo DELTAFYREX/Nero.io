068877a8e6f04c7b06799f760878875937fa7fd4
Glitch (neroio2) <none>
2024-02-19 19:34:44 +0000

    📅🏆 Checkpoint ./server/modules/network/sockets.js:31612930/146 ./public/app.js:31612930/10

--- public/app.js
+++ public/app.js
@@ -174,7 +174,7 @@ function PlaySound169() {
   
 //actually play the audio when the checkbox is clicked on (checked) and stop it when unchecked
     document.getElementById("optSound").onclick = () => {
-          global.skin = "deltaDeco";
+          global.skin = "A";
       if (document.getElementById("optSound").checked === true) {
         songrecog()
            global.music2.play()
