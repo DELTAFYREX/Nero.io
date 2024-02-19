75eb6c3407300d1afc780a036af83782a8f179d0
Glitch (neroio2) <none>
2024-02-19 14:22:46 +0000

    👬🗾 Checkpoint ./server/modules/definitions/groups/tanks.js:62478874/750 ./server/modules/definitions/addons/cooper.js:62478874/393

---
+++ server/modules/definitions/addons/cooper.js
@@ -0,0 +1,17 @@
+Class.A = {
+    PARENT: "genericTank",
+    LABEL: "Single",
+    DANGER: 7,
+    GUNS: [
+        {
+            POSITION: [19, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.single]),
+                TYPE: "bullet"
+            }
+        },
+        {
+            POSITION: [5.5, 8, -1.8, 6.5, 0, 0, 0]
+        }
+    ]
+}
