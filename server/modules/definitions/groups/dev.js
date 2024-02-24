f6ef6e55d1ddaeb18240bac3c728ae32510e2118
Glitch (neroio2) <none>
2024-02-24 15:57:42 +0000

    👨‍👨‍👦🍎 Checkpoint ./public/app.js:31612930/2524 ./server/modules/definitions/groups/dev.js:31612930/427

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -1573,6 +1573,23 @@ Class.devtesttemplate = {
         }
     ]
 };
+Class.grappeltest = {
+    PARENT: "genericTank",
+    LABEL: "Grappel",
+    DANGER: 7,
+    GUNS: [
+        {
+            POSITION: [19, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.single]),
+                TYPE: "bullet"
+            }
+        },
+        {
+            POSITION: [5.5, 8, -1.8, 6.5, 0, 0, 0]
+        }
+    ]
+};
 Class.pisseroo = {
     PARENT: ['basic'],
     LABEL: 'Winsor',
