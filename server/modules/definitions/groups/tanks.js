e302f6c301599e5568c62a35776e8f2198bb04e3
Glitch (neroio2) <none>
2024-03-03 03:12:07 +0000

    🐠🏘 Checkpoint ./server/modules/definitions/gunvals.js:31612930/66 ./server/modules/definitions/groups/tanks.js:31612930/61

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -5900,7 +5900,7 @@ Class.biggerSubduer = {
 }
 Class.accelminigun = {
     PARENT: "genericTank",
-    LABEL: "Hyper-Minigun",
+    LABEL: "Rainmaker",
     DANGER: 6,
     BODY: {
         FOV: base.FOV * 1.2
@@ -5917,21 +5917,21 @@ Class.accelminigun = {
         {
             POSITION: [21, 8, 1, 0, 0, 0, 0],
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.minigun]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.rainmaker]),
                 TYPE: ["bullet", { MOTION_TYPE: "accelerate" }]
             }
         },
         {
             POSITION: [19, 8, 1, 0, 0, 0, 1/3],
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.minigun]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.rainmaker]),
                 TYPE: ["bullet", { MOTION_TYPE: "accelerate" }]
             }
         },
         {
             POSITION: [17, 8, 1, 0, 0, 0, 2/3],
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.minigun]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.rainmaker]),
                 TYPE: ["bullet", { MOTION_TYPE: "accelerate" }]
             }
         }
